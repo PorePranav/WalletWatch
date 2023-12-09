8597334e4b23b2ca8205413dfc6dfb6b566e1294
Pranav Pore <pranav@pranavpore.com>
2023-12-09 18:05:52 +0530

    front-end basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-500 h-screen w-screen">
+      <h1 className="text-white text-3xl text-center p-2">WalletWatch</h1>
+    </div>
+  );
+}
+
+export default App;
